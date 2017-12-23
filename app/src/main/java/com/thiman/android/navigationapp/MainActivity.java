4a033939df0294e83c3aa2fa0dd40ea6ab971a07
Thimankrishh <mysoftware15@gmail.com>
2017-12-23 14:55:29 +0530

    Initial commit

---
+++ app/src/main/java/com/thiman/android/navigationapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.thiman.android.navigationapp;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private Toolbar mToolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        mToolbar = (Toolbar)findViewById(R.id.nav_action);  // set action bar overlay the navaigation view
+//        setSupportActionBar(mToolbar);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);  // set up action bar and toggle button
+
+
+    }
+    // set up button to show menu in navigation drawer
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
